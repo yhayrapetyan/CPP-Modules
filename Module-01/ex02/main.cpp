d060b43c0f889c42f5755e39bb25cfeb9f256bdb
Yurik Hayrapetyan <yuhayrap@c2r18s3.42yerevan.am>
2024-07-04 15:34:13 +0400

    ex02

---
+++ Module-01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuhayrap <yuhayrap@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/04 15:28:54 by yuhayrap          #+#    #+#             */
+/*   Updated: 2024/07/04 15:32:54 by yuhayrap         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string	brain = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &brain;
+	std::string	&stringREF = brain;
+
+	std::cout << &brain << std::endl;
+    std::cout << stringPTR  << std::endl;
+    std::cout << &stringREF << std::endl;
+    std::cout << brain << std::endl;
+    std::cout << *stringPTR  << std::endl;
+    std::cout << stringREF  << std::endl;
+
+	return 0;
+}
