a91503150707974a48ea25b1a64dc6e27dad2d55
Yurik Hayrapetyan <yuhayrap@c2r1s4.42yerevan.am>
2024-09-25 14:32:53 +0400

    fix module-07

--- Module-07/ex00/whatever.hpp
+++ Module-07/ex00/whatever.hpp
@@ -4,8 +4,8 @@
 #include <iostream>
 #include <utility>
 
-template <typename T> void swap ( T &a, T &b ) {T c(a); a=b; b=c;}
-template <typename T> const T &max (const T &a, const T &b) {return (b < a)? a:b;}
-template <typename T> const T &min (const T &a, const T &b) {return (b > a)? a:b;}
+template <typename T> void	swap ( T &a, T &b ) {T c(a); a=b; b=c;}
+template <typename T> 		T max ( T a, T b) {return (b < a)? a:b;}
+template <typename T> 		T min ( T a, T b) {return (b > a)? a:b;}
 
 #endif
--- Module-07/ex01/iter.hpp
+++ Module-07/ex01/iter.hpp
@@ -10,4 +10,11 @@ template <typename T> void iter (T *a, size_t l, void (*f)(T&)) {
         f(a[i]);
 }
 
+template <typename T> void iter (const T *a, size_t l, void (*f)(const T&)) {
+	if (a == NULL || f == NULL)
+		return;
+    for (size_t i = 0; i < l; i++)
+        f(a[i]);
+}
+
 #endif
